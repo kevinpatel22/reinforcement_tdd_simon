186b46a50fcc0faed737180c12c90ced3ba1d5a3
Kevin <mail.kevinpatel@gmail.com>
2019-07-05 16:31:58 -0400

    all test passed

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,24 @@
+const echo = (word) => {
+  return word;
+};
+
+const shout = (word) => {
+  return word.toUpperCase();
+};
+
+const repeat = (word, num) => {
+  return ((word + ' ').repeat(num).trim());
+};
+
+const startOfWord = (word, num) => {
+  return word.substring(word, num);
+};
+
+const firstWord = (word, num) => {
+  return word.split(' ')[0];
+};
+
+
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
+
